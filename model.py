aeb10b70ecf7f97ef8964b3a17d2ddef84f7b455
Olha Voichyk <66120179+Riklia@users.noreply.github.com>
2024-03-19 22:01:58 +0100

    experiment with the QNetwork

--- model.py
+++ model.py
@@ -9,10 +9,15 @@ class QNetwork(nn.Module):
         self.layer1 = nn.Linear(n_observations, 64)
         self.layer2 = nn.Linear(64, 128)
         self.layer3 = nn.Linear(128, 64)
-        self.layer4 = nn.Linear(64, n_actions)
+        self.layer4 = nn.Linear(64, 32)
+        self.layer5 = nn.Linear(32, n_actions)
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         x = F.relu(self.layer3(x))
-        return self.layer4(x)
+        x = self.dropout(x)
+        x = F.relu(self.layer4(x))
+        return self.layer5(x)
+        
