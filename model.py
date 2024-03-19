b2b40b35c966be3df81a768313fde6fdeeb99608
Olha Voichyk <66120179+Riklia@users.noreply.github.com>
2024-03-19 23:10:43 +0100

    experiment with the network

--- model.py
+++ model.py
@@ -3,15 +3,17 @@ import torch.nn.functional as F
 
 
 class QNetwork(nn.Module):
-
     def __init__(self, n_observations, n_actions):
         super(QNetwork, self).__init__()
         self.layer1 = nn.Linear(n_observations, 64)
         self.layer2 = nn.Linear(64, 128)
-        self.layer3 = nn.Linear(128, 64)
-        self.layer4 = nn.Linear(64, 32)
-        self.layer5 = nn.Linear(32, n_actions)
+        self.layer3 = nn.Linear(128, 264)
+        self.layer3 = nn.Linear(264, 528)
         self.dropout = nn.Dropout(p=0.2)
+        self.layer4 = nn.Linear(528, 264)
+        self.layer5 = nn.Linear(264, 128)
+        self.layer6 = nn.Linear(128, 64)
+        self.layer7 = nn.Linear(64, n_actions)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
@@ -19,5 +21,6 @@ class QNetwork(nn.Module):
         x = F.relu(self.layer3(x))
         x = self.dropout(x)
         x = F.relu(self.layer4(x))
-        return self.layer5(x)
-        
+        x = F.relu(self.layer5(x))
+        x = F.relu(self.layer6(x))
+        return self.layer7(x)
