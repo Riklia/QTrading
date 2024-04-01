09aa501f89378077b8cf8990a529b0db19e0debb
olhav <olgavoychik2002@gmail.com>
2024-04-01 19:59:01 +0200

    add additional configurations and some code fefactor

--- configurations/config.py
+++ configurations/config.py
@@ -24,9 +24,13 @@ class EnvParameters:
     # how many previous prices take into consideration
     window: int
     initial_balance: float
+    # balance logs from environment - experimental feature
     record_balance: bool = False
     # set step (0, 1) if you want to be able to sell/buy using some percentage of current balance
     action_step_size: float = 1
+    # if current balance < initial balance * terminate_threshold => episode is terminated
+    terminate_threshold: float = 0.05
+    transaction_fee: float = 0.01
 
 
 @dataclass(slots=True)
